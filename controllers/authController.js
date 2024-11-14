a58273bb03b071a9d7f8def6b5dd93e12cddbc48
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-11-14 22:23:53 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,6 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    sameSite: "none",
     httpOnly: true,
   };
 
